fix normalization of angular scattering; finish reproduction of Shortell et al.

Mie_angular_scattering.py
@@ -26,8 +26,14 @@ d_nm = 150.
 
 teta, Ipar, Iperp, Iunpol = Mie_tetascan(wvln_nm, d_nm,
                                          material = JC_gold(),
-                                         n_medium=1.33,
+                                         n_medium = 1.33,
                                          normalize = True)
+
+mu = np.cos(teta)
+total_teta = 2 * np.pi * np.trapezoid(Iunpol*np.sin(teta), teta)
+total_mu = -2 * np.pi * np.trapezoid(Iunpol, mu) # minus because of inversed limits
+print('Full sphere integral: ', total_teta, total_mu )
+print()
 
 plt.figure(1)
 plt.clf()
@@ -55,8 +61,14 @@ teta, Ipar, Iperp, Iunpol = Mie_tetascan(wvln_nm, d_nm,
                                          n_medium=1.33,
                                          normalize = True)
 
-K = 1/(3*pi) # normalize Rayleigh to integral = 0.5
-Icos2 = K*(1+cos(teta)**2)
+mu = np.cos(teta)
+total_teta = 2 * np.pi * np.trapezoid(Iunpol*np.sin(teta), teta)
+total_mu = -2 * np.pi * np.trapezoid(Iunpol, mu) # minus because of inversed limits
+print('Full sphere integral: ', total_teta, total_mu )
+print()
+
+K1 = 16*pi/3 # normalize Rayleigh integral to unity for full sphere
+Icos2 = (1+cos(teta)**2)/K1
 
 plt.figure(2)
 plt.clf()
@@ -77,7 +89,9 @@ plt.legend()
 
 
 
-# Not normalized for comparison: the overall shape should not change
+
+
+# Unnormalized calculation for comparison: the overall shape should not change
 
 teta, Ipar, Iperp, Iunpol = Mie_tetascan(wvln_nm, d_nm,
                                          material = Material(1.47),

miniMie/core.py
@@ -145,20 +145,30 @@ def Mie_tetascan(wvln_nm, d_nm, material=Material(1.5), n_medium=1.33,
     m = ncmplx/n_medium
     
     teta = np.linspace(0, pi, Npts)
+    mu = cos(teta)
     SL = np.zeros_like(teta)
     SR = np.zeros_like(teta)
-    for j, u in enumerate(cos(teta)):
+    for j, u in enumerate(mu):
         S1, S2 = mie_s12(m, x, u)
         SL[j] = (S1*S1.conj()).real
         SR[j] = (S2*S2.conj()).real
     SU = (SL+SR)/2
     if normalize:
-        # Integral of unpolarized intensity from 0...pi should equal 0.5
-        # (half circle)
-        SUintg = 2*np.trapezoid(SU, teta)
+        # 
+        # Full-sphere integral of unpolarized intensity will equal 1
+        #
+        # This integral is calculated as:
+        #      `total = 2 * np.pi * np.trapezoid(intensity, mu)`
+        # https://miepython.readthedocs.io/en/stable/03a_normalization.html#Verifying-normalization
+        #
+        SUintg = -2*pi*np.trapezoid(SU, mu) # minus due to inverted intg limits
         SL /= SUintg
         SR /= SUintg
         SU /= SUintg
+        # 
+        # We could also normalize to 1 by dividing by 4*pi*x**2*Qsca,
+        # but this would require a separate, explicit calculation of Qsca
+        #
     return (teta, SL, SR, SU)
 
 